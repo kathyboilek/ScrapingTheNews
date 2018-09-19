a4611719453c73a899a26b924f7ae513e54fcb7a
Kathleen Boilek <kaboilek@gmail.com>
2018-09-19 14:45:36 -0400

    updated link

--- controllers/news.js
+++ controllers/news.js
@@ -16,7 +16,8 @@ var Articles = require("../models/articles");
 var Comments = require("../models/comments");
 
 // Website To Be Scraped
-var url = "http://www.goodnewsnetwork.org/latest-news/";
+// var url = "http://www.goodnewsnetwork.org/latest-news/";
+var url = "https://www.msn.com/en-us/news/good-news";
 
 // Test Route To Verify Scraping Works From Route
 router.get('/test', function(req, res) {
